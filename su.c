ad770bea2ea03268479b83bee605ffb7b6723fd6
teddythibeault <teddythibeault@gmail.com>
2021-05-09 23:25:51 -0400

    Updated for username

--- su.c
+++ su.c
@@ -5,11 +5,11 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc < 2){
-		printf(2, "Usage: su <username>");
+	if(argc < 3){
+		printf(2, "Usage: su <username> <password>");
 	}
 
-	if(su(argv[1]) < 0){
+	if(exec("su", argv) < 0){
 		printf(2, "su: failed to change user to %s\n", argv[1]);
 	} else{
 		printf(2, "Successfully changed to user %s\n", argv[1]);
