1ce385e5e05b7979ebfbc670481685b85c9a290b
Luke <luke@laar.us>
2021-05-10 15:18:21 -0400

    fixed su

--- su.c
+++ su.c
@@ -13,16 +13,18 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc < 3)
+	if(argc < 2)
 	{
-		printf(2, "Usage: su <username> <password>");
+		printf(2, "Usage: su <username>");
+		return -1;
 	}
 
-	if(exec("su", argv) < 0){
+	if(su(argv[1]) < 0)
+	{
 		printf(2, "su: failed to change user to %s\n", argv[1]);
-	} else{
-		printf(2, "Successfully changed to user %s\n", argv[1]);
+		return -1;
 	}
 
-	exit();
+	printf(2, "Successfully changed to user %s\n", argv[1]);
+	return 0;
 }
