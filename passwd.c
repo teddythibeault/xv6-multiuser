93af1df2deb37f0d5be12a645700f1755ad5b280
teddythibeault <teddythibeault@gmail.com>
2021-05-09 23:23:01 -0400

    Add passwd user file

--- passwd.c
+++ passwd.c
@@ -5,5 +5,16 @@
 
 int main(int argc, char *argv[])
 {
+	if(argc < 2){
+		printf(2, "Usage: passwd <password>");
+		exit();
+	}	
 
+	if(passwd(argv[1]) < 0){
+		printf(2, "passwd: failed to change password");
+		break;
+	} else{
+		printf(2, "Successfully changed password");
+	
+	exit();
 }
