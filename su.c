fab27f3bfd5e10cc37e38f6697b22b50b157dd85
teddythibeault <teddythibeault@gmail.com>
2021-05-09 23:09:59 -0400

    Add user su.c call

--- su.c
+++ su.c
@@ -5,5 +5,15 @@
 
 int main(int argc, char *argv[])
 {
+	if(argc < 2){
+		printf(2, "Usage: su <username>");
+	}
 
+	if(su(argv[1]) < 0){
+		printf(2, "su: failed to change user to %s\n", argv[1]);
+	} else{
+		printf(2, "Successfully changed to user %s\n", argv[1]);
+	}
+
+	exit();
 }
