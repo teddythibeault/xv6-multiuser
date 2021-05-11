76d309fb23d5b6a1d431647b9aaad3c685765eb8
Luke <luke@laar.us>
2021-05-10 20:09:23 -0400

    f

--- w.c
+++ w.c
@@ -7,8 +7,9 @@
 int main(int argc, char *argv[])
 {
 	struct user* arg = (struct user*) malloc(sizeof(struct user));
+	char *username = w();
 
-	get_user(arg, w());
+	get_user(arg, username);
 
 	printf(1, "The current user is %s.\n The user %s last logged in on %d/%d/%d %d:%d:%d", arg->username, arg->username, arg->last_login->month,
 										arg->last_login->day, arg->last_login->year, arg->last_login->hour,
