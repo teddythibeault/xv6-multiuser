e9c22ddde8eb9e44c903c513fbe131c6cf2a578f
Luke <luke@laar.us>
2021-05-10 20:20:57 -0400

    f

--- w.c
+++ w.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
 	get_user(arg, username);
 
-	printf(1, "The current user is %s.\n The user %s last logged in on %d/%d/%d %d:%d:%d", arg->username, arg->username, arg->last_login->month,
+	printf(1, "The current user is %s.\n The user %s last logged in on %d/%d/%d %d:%d:%d", username, arg->username, arg->last_login->month,
 										arg->last_login->day, arg->last_login->year, arg->last_login->hour,
 										arg->last_login->minute, arg->last_login->second);
 
